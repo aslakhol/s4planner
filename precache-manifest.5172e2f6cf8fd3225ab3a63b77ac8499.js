4baac4e13ec48b81ceeee12c218b6996827c3a01
Aslak Hollund <aslakhol@gmail.com>
2019-12-24 15:29:54 +0100

    Updates

---
+++ precache-manifest.5172e2f6cf8fd3225ab3a63b77ac8499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04061e5b5ab5c12828b5582a0c2a1a7",
+    "url": "/s4planner/index.html"
+  },
+  {
+    "revision": "3613a9da3c688c891e8e",
+    "url": "/s4planner/static/css/main.0c9fce89.chunk.css"
+  },
+  {
+    "revision": "8ad93606d69fa7fb79c3",
+    "url": "/s4planner/static/js/2.d291c758.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/s4planner/static/js/2.d291c758.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3613a9da3c688c891e8e",
+    "url": "/s4planner/static/js/main.b41328ec.chunk.js"
+  },
+  {
+    "revision": "747539d44f1d1725afdc",
+    "url": "/s4planner/static/js/runtime-main.779feb89.js"
+  }
+]);
